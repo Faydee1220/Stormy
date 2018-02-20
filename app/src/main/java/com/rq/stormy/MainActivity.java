0d321c249566f7230a5837a9b6491032e2ade50a
Faydee <faydee1220@gmail.com>
2018-02-20 15:22:05 +0800

    新增提示錯誤對話框
    改用非同步處理、允許網路連線

--- app/src/main/java/com/rq/stormy/MainActivity.java
+++ app/src/main/java/com/rq/stormy/MainActivity.java
@@ -2,12 +2,65 @@ package com.rq.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "7dfca59c9f65002ef118da998903d236";
+        double latitude = 37.8267;
+        double longtitude = -122.4233;
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longtitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+        // 使用非同步方式在背景執行緒呼叫 API
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+                    if (response.isSuccessful()) {
+
+                    }
+                    else {
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main UI is running");
+
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
